f501429c844ff18b432491d645323e977b22f9cf
Vivek <vivekkamble762033@gmail.com>
2026-08-29 19:35:39 +0530

    added arithmetic operators

---
+++ 06.arithmetic_operators/op.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    using std::cout;
+    int students = 20;
+    // students += 1;
+    // students++; #increment operator.
+
+    // students -= 1;
+    // students --;
+
+
+    // students *= 1;
+
+    // students /= 2;
+
+    // double students /= 3;
+    double remainder = students % 2;
+
+
+    cout << students << "\n";
+    cout << remainder << "\n";
+
+    return 0;
+}
---
+++ 06.arithmetic_operators/order.md
@@ -0,0 +1,5 @@
+# Order of Precedence :
+
+1. paranthesis
+2. multiplication & division
+3. addition & subtraction
