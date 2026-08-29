2cd4d9d31c16f4b683926b03a094084c1ef3b280
Vivek <vivekkamble762033@gmail.com>
2026-08-29 19:38:42 +0530

    added new stuff

--- 06.arithmetic_operators/op.cpp
+++ 06.arithmetic_operators/op.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     using std::cout;
-    int students = 20;
+    // int students = 20;
     // students += 1;
     // students++; #increment operator.
 
@@ -15,11 +15,14 @@ int main() {
     // students /= 2;
 
     // double students /= 3;
-    double remainder = students % 2;
+    // double remainder = students % 2;
 
 
-    cout << students << "\n";
-    cout << remainder << "\n";
+    // cout << students << "\n";
+    // cout << remainder << "\n";
+    int students = 20 * 1/ 2 + 5 - 3;
+    cout << students;
 
     return 0;
 }
+
