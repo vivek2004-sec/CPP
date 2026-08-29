189886c7ac6aac7329b1061e3dc9d7d7c6238f57
Vivek <vivekkamble762033@gmail.com>
2026-08-29 22:16:47 +0530

    made some changes;

--- 06.arithmetic_operators/op.cpp
+++ 06.arithmetic_operators/op.cpp
@@ -2,26 +2,27 @@
 
 int main() {
     using std::cout;
-    // int students = 20;
-    // students += 1;
-    // students++; #increment operator.
+//     // int students = 20;
+//     // students += 1;
+//     // students++; #increment operator.
 
-    // students -= 1;
-    // students --;
-
-
-    // students *= 1;
-
-    // students /= 2;
-
-    // double students /= 3;
-    // double remainder = students % 2;
+//     // students -= 1;
+//     // students --;
 
 
-    // cout << students << "\n";
-    // cout << remainder << "\n";
-    int students = 20 * 1/( 2 + 5) - 3;
-    cout << students;
+//     // students *= 1;
+
+//     // students /= 2;
+
+//     // double students /= 3;
+//     // double remainder = students % 2;
+
+
+//     // cout << students << "\n";
+//     // cout << remainder << "\n";
+//     int students = 20 * 1/( 2 + 5) - 3;
+//     cout << students;
+
 
     return 0;
 }
