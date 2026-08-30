548063e254569edd7617ae8b32bbe3a3f773aa48
Vivek <vivekkamble762033@gmail.com>
2026-08-30 15:29:54 +0530

    added new stuff

--- 10.if_statement/if.cpp
+++ 10.if_statement/if.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 
+// int main()
+// {
+
+//     int age;
+//     std::cout << "Enter your age: ";
+//     std::cin >> age;
+
+//     if (age >= 18)
+//     {
+//         std::cout << "Welcome!";
+//     }
+//     else if (age >= 12)
+//     {
+//         std::cout << "You need parents with you.";
+//     }
+
+//     else
+//     {
+//         std::cout << "You are underage.";
+//     }
+
+//     return 0;
+// }
+
 int main()
 {
 
-    int age;
-    std::cout << "Enter your age: ";
-    std::cin >> age;
+    using namespace std;
+    int x;
+    cout << "enter the value of x: ";
+    cin >> x;
 
-    if (age >= 18)
+    if (x % 2 == 0)
     {
-        std::cout << "Welcome!";
+        cout << "It's an even number.";
     }
-    else if (age >= 12)
-    {
-        std::cout << "You need parents with you.";
-    }
-
     else
     {
-        std::cout << "You are underage.";
+        cout << "it's an odd number.";
     }
 
     return 0;
