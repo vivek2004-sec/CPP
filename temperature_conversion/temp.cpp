f8f7b006d1ebab1844d9f69ea52846130bc61ffe
Vivek <vivekkamble762033@gmail.com>
2026-08-30 18:23:52 +0530

    added new stuff

---
+++ temperature_conversion/temp.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+
+    double temp;
+    char unit;
+
+    std::cout << "***** Temperature Conversion *****\n";
+
+    std::cout << "F = Farenheit.\n";
+    std::cout << "C = Celsius.\n";
+    std::cout << "Which unit you would prefer?\n";
+    std::cin >> unit;
+
+    if (unit == 'F' || unit == 'f')
+    {
+        std::cout << "You are converting to Farenheit.\n";
+        std::cout << "Enter the temperature: ";
+        std::cin >> temp;
+        temp = (1.8 * temp) + 32.0;
+        std::cout << "Temperature is " << temp << "F\n";
+    }
+    else if (unit == 'C' || unit == 'c')
+    {
+        std::cout << "You are converting to Celcius.\n";
+        std::cout << "Enter the temperature: ";
+        std::cin >> temp;
+        temp = (temp - 32) / 1.8;
+        std::cout << "Temperature is " << temp << "C\n";
+    }
+
+    std::cout << "\n**********************************";
+
+    return 0;
+}
