72c93e278fe2600325dfa7bcb3f9576bd5db2144
Vivek <vivekkamble762033@gmail.com>
2026-08-30 17:40:31 +0530

    added new stuff

--- 12.ternary_operator/op.cpp
+++ 12.ternary_operator/op.cpp
@@ -6,9 +6,12 @@ int main()
     // int grade = 70;
     // grade >= 45 ? std::printf("You've passed!") : std::printf("You've Failed.");
 
-    int number;
-    std::printf("Enter the number: ");
-    std::cin >> number;
-    number % 2 == 0 ? std::cout << "Even" : std::cout << "Odd";
+    // int number;
+    // std::printf("Enter the number: ");
+    // std::cin >> number;
+    // number % 2 == 0 ? std::cout << "Even" : std::cout << "Odd";
+
+    bool hungry = false;
+    std::cout << (hungry ? "you are hungry" : "You are full.");
     return 0;
 }
