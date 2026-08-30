262db976d9bf8a3fadd7467373c6833022bfb40b
Vivek <vivekkamble762033@gmail.com>
2026-08-30 13:45:32 +0530

    added new stuff

--- 09.math/main.cpp
+++ 09.math/main.cpp
@@ -6,7 +6,7 @@ int main () {
     using std::cout;
     using std::cin;
 
-    int x = 3;
+    int x = 3.123;
     int y = 55;
     int z;
 
@@ -14,7 +14,11 @@ int main () {
 
     // z = std::min(x, y);
     // z = pow(2, x);
-    z = sqrt(9);
+    // z = sqrt(9);
+    // z = abs(  -3);
+    // z = round(x);
+    // z = ceil(x);
+    z = floor(x);
 
     cout << z;
 
