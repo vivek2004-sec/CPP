9552cd600d41beecd3b135f2c6e31154a5caaa30
Vivek <vivekkamble762033@gmail.com>
2026-08-31 23:05:51 +0530

    added new stuff

--- 19.nested_loop/loop.cpp
+++ 19.nested_loop/loop.cpp
@@ -2,13 +2,35 @@
 
 int main()
 {
-    for (int i = 1; i <= 5; i++)
+    // for (int i = 1; i <= 5; i++)
+    // {
+    //     for (int j = 1; j <= 10; j++)
+    //     {
+    //         std::cout << j;
+    //     }
+    //     std::cout << "\n";
+    // }
+    int rows;
+    int columns;
+    char symbols;
+
+    std::cout << "Enter the number of rows: ";
+    std::cin >> rows;
+
+    std::cout << "Enter the number of columns: ";
+    std::cin >> columns;
+
+    std::cout << "Enter the symbol: ";
+    std::cin >> symbols;
+
+    for (int i = 1; i <= rows; i++)
     {
-        for (int j = 1; j <= 10; j++)
+        for (int j = 1; j <= columns; j++)
         {
-            std::cout << j;
+            std::cout << symbols;
         }
         std::cout << "\n";
     }
-    return 0;
+
+        return 0;
 }
