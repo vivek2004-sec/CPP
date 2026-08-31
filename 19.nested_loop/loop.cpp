343f9f80312834b488d6e7bd427d27f69467df57
Vivek <vivekkamble762033@gmail.com>
2026-08-31 22:57:36 +0530

    added new stuff

---
+++ 19.nested_loop/loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            std::cout << j;
+        }
+    }
+    return 0;
+}
