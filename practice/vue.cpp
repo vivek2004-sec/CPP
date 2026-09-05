aa0fbaaa0ae814eec1f845c834db0a4a167f5d19
Vivek <vivekkamble762033@gmail.com>
2026-09-05 19:16:46 +0530

    added new stuff

--- practice/vue.cpp
+++ practice/vue.cpp
@@ -1,35 +1,17 @@
 #include <iostream>
 
+namespace first_name
+{
+    std::string name = "vivek";
+}
 
-int main() {
-    int num = 10;
-    int days = 25;
-    int hours = 2.33;
+int main()
+{
 
-    std::cout << num << std::endl;
-    std::cout << days << std::endl;
-    std::cout << hours << std::endl;
-    
+    std::string name = "rishi";
 
+    std::cout << first_name::name << std::endl;
+    std::cout << name;
 
-    double marks = 2.33;
-    double gpa = 7.5;
-    double digits = 8.989;
-
-    std::cout << marks << std::endl;
-    std::cout << gpa << std::endl;
-    std::cout << digits << std:: endl;
-
-
-    char a = 'A';
-    std::cout << a << "\n";
-
-
-    std::string name = "Vivek";
-    std::cout << name << "\n";   
-
-
-    bool student = true;
-    std::cout << student << "\n";
     return 0;
 }
