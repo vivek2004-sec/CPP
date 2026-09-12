c878df03e58caf8e456fabc0b2cfa9386bfd9e15
Vivek <vivekkamble762033@gmail.com>
2026-09-13 00:24:51 +0530

    added new stuff

--- 20.functions/function.cpp
+++ 20.functions/function.cpp
@@ -2,15 +2,25 @@
 
 void happy(std::string name);
 
+void sum(int a, int b);
+
 int main()
 {
     std::string name;
     std::cin >> name;
     happy(name);
+    sum(3, 4);
     return 0;
 }
 
 void happy(std::string name)
 {
     std::cout << "Happy Birthday! " << name;
+}
+
+void sum(int a, int b)
+{
+
+    int sum = a + b;
+    std::cout << sum;
 }
